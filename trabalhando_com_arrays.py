0ae560ada12148039b872e8405227f339a9cb656
Matheus Costa <63138278+matheuscosta2@users.noreply.github.com>
2023-11-19 19:18:17 -0300

    Create trabalhando_com_arrays.py

---
+++ trabalhando_com_arrays.py
@@ -0,0 +1,8 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). 
+# O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). 
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável, 
+# e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+lista_de_produtos_amazon = ["Echo Dot 3", "Kindle", "Amazfit", "Echo pop", "Eco Show" ]
+lista_de_anos_nascimento = [1994, 1995, 1973, 1910, 1989 ]
+print(lista_de_produtos_amazon, lista_de_anos_nascimento)
